4bf924ccc7d7179d23e2edd5ba5ac9eea3db792e
Daniel Haven <d-haven@outlook.com>
2024-11-15 20:45:11 -0500

    Update ynab-sdk-laravel.php

--- config/ynab-sdk-laravel.php
+++ config/ynab-sdk-laravel.php
@@ -2,5 +2,11 @@
 
 // config for DanielHaven/YnabSdkLaravel
 return [
-
+    'base_url' => 'https://api.ynab.com/v1',
+    'client' => [
+        'id' => env('YNAB_SDK_LARAVEL_CLIENT_ID'),
+        'secret' => env('YNAB_SDK_LARAVEL_CLIENT_SECRET'),
+    ],
+    'redirect_uri' => env('YNAB_SDK_LARAVEL_REDIRECT_URI', 'http://localhost'),
+    'response_type' => env('YNAB_SDK_LARAVEL_RESPONSE_TYPE', 'code'),
 ];
